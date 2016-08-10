541eca4f2ca24448960d38662b378e1298c39b00
Mike Ball <mikedball@gmail.com>
2016-08-10 09:43:34 -0400

    tweak deploy script a bit

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,9 @@ cp -R ../images .
 cp -R ../videos .
 
 echo "Pushing to gh-pages"
-git init
 git config user.name "Travis-CI"
+git config user.name "mikedball@gmail.com"
+git init
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
