6dc10824337b27c318c367a1a3e00b5054d83342
Mike Ball <mikedball@gmail.com>
2016-08-12 07:22:16 -0400

    add another example test

--- test/index_spec.js
+++ test/index_spec.js
@@ -7,4 +7,10 @@ describe('presentation', function() {
   it('should have the correct title', function() {
     expect(browser.getTitle()).toEqual('Headless Testing Against Real Web Browsers');
   });
+
+  it('should advance slides when "next" is clicked', function() {
+    element(by.css('div.arrow.next')).click();
+
+    expect(element(by.id('before-we-get-started-a-challenge')).isDisplayed()).toBeTruthy();
+  });
 });
